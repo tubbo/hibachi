96c7231676f0e97fafab57e57f7a608608c16f98
Tom Scott <tscott@telvue.com>
2014-06-08 13:18:54 -0400

    Make MockSetting more sane

--- spec/support/mock_setting.rb
+++ spec/support/mock_setting.rb
@@ -4,10 +4,9 @@ class MockSetting < Hibachi::Model
 
   recipe :mock_settings
 
-  attr_accessor :attributes, :name, :status, :is_active
+  attr_accessor :attributes, :name, :is_active
 
   validates :name, :presence => true
-  validates :status, :presence => true, :if => :active?
 
   def active?
     !!is_active
