8bd6f3f6ceead9617bfa54d77a4b08f605c0399e
Tom Scott <tscott@telvue.com>
2014-06-08 13:19:08 -0400

    Add querying to Store

--- lib/hibachi/store.rb
+++ lib/hibachi/store.rb
@@ -1,7 +1,6 @@
-require 'hibachi/node'
-
 module Hibachi
-  module Persistence
+  # Backend data store methods for the Hibachi::Model
+  module Store
     extend ActiveSupport::Concern
 
     module ClassMethods
@@ -10,6 +9,32 @@ module Hibachi
         model = new from_attributes
         model.save
         model
+      end
+
+      # Find everything from the JSON.
+      def all
+        node[recipe].map { |from_params| new from_params }
+      end
+
+      # Find all objects of this type matching the given search
+      # criteria. Uses the all() method to scope calls from within the
+      # proper JSON for this class, and instantiates objects based on
+      # the found information, so you get back an Array of whatever
+      # object this model happens to be. If none, this returns an empty
+      # Array.
+      def where has_attributes={}
+        all.select { |persisted_attr, persisted_val|
+          has_attributes.any? { |search_attr, search_val|
+            persisted_attr == search_attr && persisted_val == search_val
+          }
+        }.map { |from_params|
+          new from_params
+        }
+      end
+
+      # Find a given model by name, or return `nil`.
+      def find by_name
+        where(name: by_name).first
       end
     end
 
