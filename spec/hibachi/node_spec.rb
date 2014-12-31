8d5a03820bb11d1c4955a2f2142b0d0849d491bf
Tom Scott <tscott@telvue.com>
2014-12-30 19:15:37 -0500

    Fix tests

--- spec/hibachi/node_spec.rb
+++ spec/hibachi/node_spec.rb
@@ -3,49 +3,65 @@ require 'hibachi/node'
 
 module Hibachi
   describe Node do
-    subject do
-      fixture_path = File.expand_path "../../fixtures", __FILE__
-      Node.find "#{fixture_path}/chef.json"
+    let :node do
+      double 'Ridley::Node', :attributes => {
+        :chef_environment => 'staging'
+      }
     end
 
-    it "finds the file at path" do
-      expect(subject).to be_present
+    let :collection do
+      double 'Ridley::Collection'
     end
 
-    it "finds attributes in the file" do
-      expect(subject.send(:parsed_json_attributes)).to be_present
-      expect(subject.send(:parsed_json_attributes)).to_not be_empty
-      expect(subject.send(:parsed_json_attributes).keys).to include(Hibachi.config.cookbook.to_s)
+    let :client do
+      double 'Ridley', :node => collection
     end
 
-    it "scopes attributes by cookbook" do
-      expect(subject.attributes).to be_present
-      expect(subject.attributes).to_not be_empty
+    before do
+      allow(collection).to receive(:find).with('node.teve.inc').and_return node
+      allow(Ridley).to recieve(:new).and_return client
     end
 
-    it "enumerates over json attributes" do
-      expect(subject).to respond_to :each
+    subject { Node.new 'node.teve.inc' }
+
+    it "initializes with a name" do
+      expect(subject.name).to eq('node.teve.inc')
     end
 
-    it "finds a given recipe's json" do
-      expect(subject[:mock_settings]).to_not be_empty
+    it "finds by name" do
+      expect(Node.find('node.teve.inc')).to_not be_nil
     end
 
-    it "exposes attributes as a hash" do
-      expect(subject.attributes[:mock_settings]).to eq(subject[:mock_settings])
+    it "tests whether a node exists" do
+      expect(Node.exists?('node.teve.inc')).to eq(true)
+      expect(Node.exists?('example.teve.inc')).to eq(false)
     end
 
-    context "when writing to json" do
-      before { subject[:from_node_spec] = { test: true } }
+    it "instantiates a new chef client" do
+      expect(Node.chef).to be_a Ridley
+    end
 
-      it "sets recipe json" do
-        expect(subject[:from_node_spec]).to_not be_nil
-        expect(subject[:from_node_spec][:test]).to eq(true)
+    it "sets node attributes" do
+      expect(subject.set(attribute: 'value')).to eq(true)
+      expect(subject).to respond_to(:[]=)
+    end
+
+    it "gets node attributes" do
+      expect(subject.get(:chef_environment)).to eq('staging')
+      expect(subject[:chef_environment]).to eq('staging')
+    end
+
+    context "when saving to the chef server" do
+      before do
+        allow(subject.send(:chef).to receive(:update).and_return true
       end
 
-      it "deletes recipe json" do
-        expect(subject.delete(:from_node_spec)).to eq(true)
-        expect(subject[:from_node_spec]).to be_nil
+      it "updates attributes and saves" do
+        expect(subject.update_attributes(attribute: 'value')).to eq(true)
+      end
+
+      it "saves current state of object to chef" do
+        expect(subject.save).to eq(true)
       end
     end
   end
